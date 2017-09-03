f13061c716030eed3162c73dba63487d9a16d4d7
dambadipudi <divya.shastry@gmail.com>
2017-09-03 13:28:14 -0700

    Created the base file
    Added a function for generating a random location
    Added a function for generating a random color
    Added a function to draw a cicle at the location with that color

--- paint_splatter/paint_splatter.js
+++ paint_splatter/paint_splatter.js
@@ -1,1 +1,37 @@
+/**
+ * After the page loads, prepare the Canvas
+ */
+$(document).ready(function(){
+    drawOnCanvas();
+});
 
+var context;
+var paintSplatterCount = 0;
+
+function getRandomLocation() {
+    return { x_pos: Math.floor(Math.random()*context.canvas.width),
+             y_pos: Math.floor(Math.random()*context.canvas.height) };
+}
+
+function getRandomColor() {
+    // 16777216 is decimal equivalent of 0xFFFFFF
+    var color = Math.floor(Math.random() * 16777216).toString(16);
+    // Make sure to get a 6 digit number by appending extra 0s as needed
+    return '#000000'.slice(0, -color.length) + color;
+}
+
+function drawOnCanvas() {
+    context = document.getElementById('paint_splatter_canvas').getContext("2d");
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    
+    while(paintSplatterCount < 10) {
+        paintSplatterCount ++;
+        context.beginPath();
+        context.strokeStyle = getRandomColor();
+        var randomPosition = getRandomLocation();
+         //Get a normal distribution for maximum paint near the point and a few paint drop outliers as splatter
+        
+        context.arc(randomPosition.x_pos,randomPosition.y_pos,50,0,2*Math.PI); // For now just draw a circle
+        context.stroke();
+    }
+}
