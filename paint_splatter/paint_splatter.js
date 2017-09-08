c85ebe4bd8885e1f21c2e21090070d06fe35bda5
dambadipudi <divya.shastry@gmail.com>
2017-09-08 10:12:38 -0700

    Updated Gaussian rand function

--- paint_splatter/paint_splatter.js
+++ paint_splatter/paint_splatter.js
@@ -20,15 +20,29 @@ function getRandomColor() {
     // Make sure to get a 6 digit number by appending extra 0s as needed
     return '#000000'.slice(0, -color.length) + color;
 }
+var previous = false;
+var y2 = 0;
 
-function gaussianRand() {
-  var rand = 0;
+function gaussianRand(mean, std) {
+      var y1,x1,x2,w;
+      if (previous) {
+        y1 = y2;
+        previous = false;
+      } else {
+        do {
+          x1 = Math.random()*2 - 1;
+          x2 = Math.random()*2 - 1;
+          w = x1 * x1 + x2 * x2;
+        } while (w >= 1);
+        w = Math.sqrt((-2 * Math.log(w))/w);
+        y1 = x1 * w;
+        y2 = x2 * w;
+        previous = true;
+      }
 
-  for (var i = 0; i < 6; i += 1) {
-    rand += Math.random();
-  }
-
-  return rand / 6;
+      var m = mean || 0;
+      var s = std || 1;
+      return y1*s + m;
 }
 
 function drawPaintSplatter() {
