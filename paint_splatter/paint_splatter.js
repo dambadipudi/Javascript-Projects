577683e24857542e109e246c635b2cb4645a7971
dambadipudi <divya.shastry@gmail.com>
2017-09-12 10:48:31 -0700

    Added comments

--- paint_splatter/paint_splatter.js
+++ paint_splatter/paint_splatter.js
@@ -1,28 +1,58 @@
-var context;
-var paintSplatterCount = 0;
+/**
+ *   Paint_Splatter.js file is used to create a paint splatter simulation
+ *   A normal distribution is used to draw small dots for the splatter
+ *   A random location and random color is chosen for each splatter
+ *
+ *   Coded by Divya Ambadipudi for Khan Academy Advanced JS: Natural Simulations course
+ *
+ */
+
+
+
+var context; // Context for Canvas object
+var paintSplatterCount = 0; // The number of splatters for each simulation
 
 /**
- * After the page loads, prepare the Canvas
+ * After the page loads, get the Canvas context and add button event
  */
 $(document).ready(function(){
     context = document.getElementById('paint_splatter_canvas').getContext("2d");
     document.getElementById("splatter_button").addEventListener("click", drawOnCanvas);
 });
 
+/**
+ *     getRamdomLocation function generates an object with
+ *      random x and y coordinates
+ *      @return Object 
+ */
 function getRandomLocation() {
     return { x_pos: Math.floor(Math.random()*context.canvas.width*3/4),
              y_pos: Math.floor(Math.random()*context.canvas.height*3/4) };
 }
 
+/**
+ *     getRamdomColor function generates a random hexadecimal string
+ *      generated
+ *      @return String 
+ */
 function getRandomColor() {
     // 16777216 is decimal equivalent of 0xFFFFFF
     var color = Math.floor(Math.random() * 16777216).toString(16);
     // Make sure to get a 6 digit number by appending extra 0s as needed
     return '#000000'.slice(0, -color.length) + color;
 }
-var previous = false;
-var y2 = 0;
 
+var previous = false; //Variables needed for gaussianRand function
+var y2 = 0; // //Variables needed for gaussianRand function
+
+/**
+ *     gaussianRand function borrowed from processing.js library
+ *     generates random numbers from a normal distribution with given mean and std
+ *     Uses default of 0 and 1 if not specified
+ *     @param Float mean of the normal distribution required
+ *     @param Float standard deviation of the normal distribution required
+ *     @return Float 
+ */
 function gaussianRand(mean, std) {
       var y1,x1,x2,w;
       if (previous) {
@@ -45,6 +75,13 @@ function gaussianRand(mean, std) {
       return y1*s + m;
 }
 
+
+/**
+ *     drawPaintSplatter function draws a splatter of dots at random points 
+ *     of a normal distribution with current location as mean
+ *     then calls itself again after an interval to draw another
+ *     splatter at another random position
+ */
 function drawPaintSplatter() {
     paintSplatterCount ++;
     
@@ -77,9 +114,13 @@ function drawPaintSplatter() {
 
 }
 
+/**
+ *     drawOnCanvas function is called when Splatter button is clicked
+ *      
+ */
 function drawOnCanvas() {
-    paintSplatterCount = 0;
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    paintSplatterCount = 0; //Initialise the splatter count
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear the canvas
     console.log("Clearing canvas and painting again,splatter count -" + paintSplatterCount + "\n");    
-    drawPaintSplatter();
+    drawPaintSplatter(); //Call the function that draws splatters
 }
